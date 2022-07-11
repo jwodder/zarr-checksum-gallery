ac7e18c898eccdc88c67f3a4acd71be9baadc25f
John T. Wodder II <git@varonathe.org>
2022-07-11 01:02:36 -0400

    Fix walker and add an entry point

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,18 @@ use crate::checksum::{FileInfo, ZarrEntry};
 use std::path::Path;
 use walkdir::WalkDir;
 
+// TODO: Return a Result
 pub fn walkdir_checksum<P: AsRef<Path>>(dirpath: P) -> String {
     let zarr: Result<ZarrEntry, _> = WalkDir::new(dirpath.as_ref())
         .into_iter()
-        .filter_entry(|e| e.file_type().is_file())
+        // We can't use walkdir's filter_entry(), because that prevents
+        // descending into directories that don't match the predicate.
+        // We also can't use r.map() inside the filter(), as that takes
+        // ownership of r.
+        .filter(|r| match r {
+            Ok(e) => e.file_type().is_file(),
+            Err(_) => true,
+        })
         .map(|r| r.map(|e| FileInfo::for_file(e.path(), dirpath.as_ref())))
         .collect();
     match zarr {
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use dandi_zarr_checksum::walkdir_checksum;
+use std::env::args;
+
 fn main() {
-    println!("Hello, world!");
+    match args().nth(1) {
+        Some(p) => println!("{}", walkdir_checksum(p)),
+        None => panic!("No directory path provided"),
+    }
 }
