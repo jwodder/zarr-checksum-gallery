4d6830ae22bf423307971e9cece058034d611140
John T. Wodder II <git@varonathe.org>
2022-07-11 08:48:53 -0400

    Integration test for walkdir_checksum()

---
+++ tests/walkers.rs
@@ -0,0 +1,17 @@
+use dandi_zarr_checksum::walkdir_checksum;
+use std::path::PathBuf;
+
+const SAMPLE_CHECKSUM: &str = "4313ab36412db2981c3ed391b38604d6-5--1516";
+
+fn sample_path() -> PathBuf {
+    let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    path.push("tests");
+    path.push("data");
+    path.push("sample.zarr");
+    path
+}
+
+#[test]
+fn test_walkdir_checksum() {
+    assert_eq!(walkdir_checksum(sample_path()), SAMPLE_CHECKSUM);
+}
