1527a97f311d3e14049b5d81e9db50441e811284
John T. Wodder II <git@varonathe.org>
2022-07-13 22:43:48 -0400

    Add tests with empty directories

--- tests/walkers.rs
+++ tests/walkers.rs
@@ -1,5 +1,8 @@
 use dandi_zarr_checksum::{depth_first_checksum, recursive_checksum, walkdir_checksum};
+use fs_extra::dir;
+use std::fs::create_dir_all;
 use std::path::PathBuf;
+use tempfile::{tempdir, TempDir};
 
 const SAMPLE_CHECKSUM: &str = "4313ab36412db2981c3ed391b38604d6-5--1516";
 
@@ -11,9 +14,32 @@ fn sample_path() -> PathBuf {
     path
 }
 
+fn sample2() -> TempDir {
+    let tmp_path = tempdir().unwrap();
+    dir::copy(sample_path(), tmp_path.path(), &dir::CopyOptions::new()).unwrap();
+    let mut path = PathBuf::from(tmp_path.path());
+    path.push("sample.zarr");
+    path.push("arr_2");
+    create_dir_all(path).unwrap();
+    let mut path = PathBuf::from(tmp_path.path());
+    path.push("sample.zarr");
+    path.push("arr_3");
+    path.push("foo");
+    create_dir_all(path).unwrap();
+    tmp_path
+}
+
 #[test]
 fn test_walkdir_checksum() {
     assert_eq!(walkdir_checksum(sample_path()).unwrap(), SAMPLE_CHECKSUM);
+}
+
+#[test]
+fn test_walkdir_checksum2() {
+    assert_eq!(
+        walkdir_checksum(sample2().path().join("sample.zarr")).unwrap(),
+        SAMPLE_CHECKSUM
+    );
 }
 
 #[test]
@@ -22,9 +48,25 @@ fn test_recursive_checksum() {
 }
 
 #[test]
+fn test_recursive_checksum2() {
+    assert_eq!(
+        recursive_checksum(sample2().path().join("sample.zarr")).unwrap(),
+        SAMPLE_CHECKSUM
+    );
+}
+
+#[test]
 fn test_depth_first_checksum() {
     assert_eq!(
         depth_first_checksum(sample_path()).unwrap(),
+        SAMPLE_CHECKSUM
+    );
+}
+
+#[test]
+fn test_depth_first_checksum2() {
+    assert_eq!(
+        depth_first_checksum(sample2().path().join("sample.zarr")).unwrap(),
         SAMPLE_CHECKSUM
     );
 }
