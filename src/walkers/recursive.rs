73c7fe2c74a7d5127ff78cb27891462b7a75cde6
John T. Wodder II <git@varonathe.org>
2022-07-13 21:43:16 -0400

    Cut down on some type-genericness

--- src/walkers/recursive.rs
+++ src/walkers/recursive.rs
@@ -5,7 +5,7 @@ use std::fs;
 use std::path::Path;
 
 pub fn recursive_checksum<P: AsRef<Path>>(dirpath: P) -> Result<String, ZarrError> {
-    fn recurse<P: AsRef<Path>>(path: P, basepath: P) -> Result<ZarrDigest, ZarrError> {
+    fn recurse(path: &Path, basepath: &Path) -> Result<ZarrDigest, ZarrError> {
         let mut files = HashMap::new();
         let mut directories = HashMap::new();
         for p in fs::read_dir(&path).map_err(|e| ZarrError::readdir_error(&path, e))? {
@@ -15,16 +15,17 @@ pub fn recursive_checksum<P: AsRef<Path>>(dirpath: P) -> Result<String, ZarrErro
                 .map_err(|e| ZarrError::stat_error(&p.path(), e))?
                 .is_dir()
             {
-                directories.insert(name, recurse(&p.path(), &basepath.as_ref().into())?);
+                directories.insert(name, recurse(&p.path(), basepath)?);
             } else {
                 files.insert(
                     name,
-                    FileInfo::for_file(p.path(), basepath.as_ref().into())?.to_zarr_digest(),
+                    FileInfo::for_file(p.path(), basepath.into())?.to_zarr_digest(),
                 );
             }
         }
         Ok(get_checksum(files, directories))
     }
 
+    let dirpath = dirpath.as_ref().to_path_buf();
     Ok(recurse(&dirpath, &dirpath)?.digest)
 }
