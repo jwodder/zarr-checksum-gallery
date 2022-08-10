ed8c0e214f986dbf41c7f5c71ac3da4de33f1f4c
John T. Wodder II <git@varonathe.org>
2022-08-10 18:16:30 -0400

    Make more walkers follow directory symlinks

--- src/walkers/util.rs
+++ src/walkers/util.rs
@@ -13,10 +13,12 @@ pub(crate) fn listdir<P: AsRef<Path>>(dirpath: P) -> Result<Vec<DirEntry>, FSErr
     for p in fs::read_dir(&dirpath).map_err(|e| FSError::readdir_error(&dirpath, e))? {
         let p = p.map_err(|e| FSError::readdir_error(&dirpath, e))?;
         let path = p.path();
-        let is_dir = p
-            .file_type()
-            .map_err(|e| FSError::stat_error(&p.path(), e))?
-            .is_dir();
+        let ftype = p.file_type().map_err(|e| FSError::stat_error(&path, e))?;
+        let is_dir = ftype.is_dir()
+            || (ftype.is_symlink()
+                && fs::metadata(&path)
+                    .map_err(|e| FSError::stat_error(&path, e))?
+                    .is_dir());
         entries.push(DirEntry { path, is_dir });
     }
     Ok(entries)
