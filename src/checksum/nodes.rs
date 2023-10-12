2a6a7d49701bc9d7d27e06b90e6cba704e8c24da
John T. Wodder II <git@varonathe.org>
2023-10-12 12:39:39 -0400

    Dirsummer

--- src/checksum/nodes.rs
+++ src/checksum/nodes.rs
@@ -127,6 +127,72 @@ impl EntryChecksum {
     }
 }
 
+/// Struct for computing the checksum for a directory.  After creation,
+/// [`push()`][Dirsummer::push] the checksums for each directory entry and then
+/// call [`checksum()`][Dirsummer::checksum] to fetch the directory's checksum.
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub struct Dirsummer {
+    relpath: EntryPath,
+    files: Vec<FileChecksum>,
+    directories: Vec<DirChecksum>,
+    size: u64,
+    file_count: u64,
+}
+
+impl Dirsummer {
+    pub fn new(relpath: EntryPath) -> Dirsummer {
+        Dirsummer {
+            relpath,
+            files: Vec::new(),
+            directories: Vec::new(),
+            size: 0,
+            file_count: 0,
+        }
+    }
+
+    /// Return the path within the Zarr for the directory
+    pub fn relpath(&self) -> &EntryPath {
+        &self.relpath
+    }
+
+    /// Add a checksum for an entry of the directory.
+    ///
+    /// It is the caller's responsibility to ensure that `chksum` actually
+    /// belongs to an entry in the on-disk directory and that `push()` is not
+    /// called with two different checksums with the same
+    /// [`name`][Checksum::name].  If these conditions are not met,
+    /// [`checksum()`][Dirsummer::checksum] will return an inaccurate value.
+    pub fn push<N: Into<EntryChecksum>>(&mut self, chksum: N) {
+        let node = chksum.into();
+        self.size += node.size();
+        self.file_count += node.file_count();
+        match node {
+            EntryChecksum::File(f) => self.files.push(f),
+            EntryChecksum::Directory(d) => self.directories.push(d),
+        }
+    }
+
+    /// Compute the checksum for the directory based on the entry checksums
+    /// added so far
+    pub fn checksum(&self) -> DirChecksum {
+        let md5 = md5_string(&get_checksum_json(
+            self.files.iter(),
+            self.directories.iter(),
+        ));
+        let checksum = format!("{}-{}--{}", md5, self.file_count, self.size);
+        debug!(
+            "Computed checksum for directory {}: {}",
+            self.relpath, checksum
+        );
+        DirChecksum {
+            relpath: self.relpath.clone(),
+            checksum,
+            size: self.size,
+            file_count: self.file_count,
+        }
+    }
+}
+
 /// Compute the checksum for the directory at relative path `relpath` within a
 /// Zarr, where the entries of the directory have the checksums supplied in
 /// `iter`.
@@ -151,7 +217,7 @@ where
             EntryChecksum::Directory(d) => directories.push(d),
         }
     }
-    let md5 = md5_string(&get_checksum_json(files, directories));
+    let md5 = md5_string(&get_checksum_json(files.iter(), directories.iter()));
     let checksum = format!("{md5}-{file_count}--{size}");
     debug!("Computed checksum for directory {relpath}: {checksum}");
     DirChecksum {
@@ -251,5 +317,103 @@ mod test {
         ];
         let checksum = get_checksum("foo".try_into().unwrap(), nodes);
         assert_eq!(checksum.checksum, "d5e4eb5dc8efdb54ff089db1eef34119-2--2");
+    }
+
+    #[test]
+    fn test_dirsummer_nothing() {
+        let ds = Dirsummer::new("foo".try_into().unwrap());
+        assert_eq!(
+            ds.checksum().checksum,
+            "481a2f77ab786a0f45aafd5db0971caa-0--0"
+        );
+    }
+
+    #[test]
+    fn test_dirsummer_one_file() {
+        let mut ds = Dirsummer::new("foo".try_into().unwrap());
+        ds.push(FileChecksum {
+            relpath: "bar".try_into().unwrap(),
+            checksum: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa".into(),
+            size: 1,
+        });
+        assert_eq!(
+            ds.checksum().checksum,
+            "f21b9b4bf53d7ce1167bcfae76371e59-1--1"
+        );
+    }
+
+    #[test]
+    fn test_dirsummer_one_directory() {
+        let mut ds = Dirsummer::new("foo".try_into().unwrap());
+        ds.push(DirChecksum {
+            relpath: "bar".try_into().unwrap(),
+            checksum: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa-1--1".into(),
+            size: 1,
+            file_count: 1,
+        });
+        assert_eq!(
+            ds.checksum().checksum,
+            "ea8b8290b69b96422a3ed1cca0390f21-1--1"
+        );
+    }
+
+    #[test]
+    fn test_dirsummer_two_files() {
+        let mut ds = Dirsummer::new("foo".try_into().unwrap());
+        ds.push(FileChecksum {
+            relpath: "bar".try_into().unwrap(),
+            checksum: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa".into(),
+            size: 1,
+        });
+        ds.push(FileChecksum {
+            relpath: "baz".try_into().unwrap(),
+            checksum: "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb".into(),
+            size: 1,
+        });
+        assert_eq!(
+            ds.checksum().checksum,
+            "8e50add2b46d3a6389e2d9d0924227fb-2--2"
+        );
+    }
+
+    #[test]
+    fn test_dirsummer_two_directories() {
+        let mut ds = Dirsummer::new("foo".try_into().unwrap());
+        ds.push(DirChecksum {
+            relpath: "bar".try_into().unwrap(),
+            checksum: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa-1--1".into(),
+            size: 1,
+            file_count: 1,
+        });
+        ds.push(DirChecksum {
+            relpath: "baz".try_into().unwrap(),
+            checksum: "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb-1--1".into(),
+            size: 1,
+            file_count: 1,
+        });
+        assert_eq!(
+            ds.checksum().checksum,
+            "4c21a113688f925240549b14136d61ff-2--2"
+        );
+    }
+
+    #[test]
+    fn test_dirsummer_one_of_each() {
+        let mut ds = Dirsummer::new("foo".try_into().unwrap());
+        ds.push(EntryChecksum::File(FileChecksum {
+            relpath: "baz".try_into().unwrap(),
+            checksum: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa".into(),
+            size: 1,
+        }));
+        ds.push(EntryChecksum::Directory(DirChecksum {
+            relpath: "bar".try_into().unwrap(),
+            checksum: "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb-1--1".into(),
+            size: 1,
+            file_count: 1,
+        }));
+        assert_eq!(
+            ds.checksum().checksum,
+            "d5e4eb5dc8efdb54ff089db1eef34119-2--2"
+        );
     }
 }
