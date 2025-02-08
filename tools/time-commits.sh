8d429692698837115f195b99a8283d84f77c6313
John T. Wodder II <git@varonathe.org>
2025-02-07 23:14:57 -0500

    That's not right either!

--- tools/time-commits.sh
+++ tools/time-commits.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 commit1="${1:?Usage $0 <commit1> <commit2> <zarr>}"
 commit2="${2:?Usage $0 <commit1> <commit2> <zarr>}"
-zarr="${3:?Usage: $0 <commit1> <commit2> <zarr>}"
+zarrpath="${3:?Usage: $0 <commit1> <commit2> <zarr>}"
 hyperfine \
     -L commit "$commit1","$commit2" \
     -s 'git checkout {commit} && cargo build -r' \
