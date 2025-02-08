2df22ca9110a98118f4e3383ec9bc0b39383b7eb
John T. Wodder II <git@varonathe.org>
2025-02-07 23:18:38 -0500

    Give time-commits.sh an "implementation" argument

--- tools/time-commits.sh
+++ tools/time-commits.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-commit1="${1:?Usage $0 <commit1> <commit2> <zarr>}"
-commit2="${2:?Usage $0 <commit1> <commit2> <zarr>}"
-zarrpath="${3:?Usage: $0 <commit1> <commit2> <zarr>}"
+commit1="${1:?Usage $0 <commit1> <commit2> <zarr> [<implementation>]}"
+commit2="${2:?Usage $0 <commit1> <commit2> <zarr> [<implementation>]}"
+zarrpath="${3:?Usage: $0 <commit1> <commit2> <zarr> [<implementation>]}"
+implementation="${4:-fastio}"
 hyperfine \
     -L commit "$commit1","$commit2" \
     -s 'git checkout {commit} && cargo build -r' \
     -w3 \
-    -n '{commit}' "target/release/zarr-checksum-gallery fastio $zarrpath"
+    -n '{commit}' "target/release/zarr-checksum-gallery $implementation $zarrpath"
